900fd29f852d64800fc6527930a5cae89f2ee892
Jelmer Vernooij <jelmer@samba.org>
2010-09-04 16:33:18 +0200

    Add testrepository configuration, test_suite() function.

---
+++ fastimport/tests/__init__.py
@@ -0,0 +1,35 @@
+# __init__.py -- The tests for python-fastimport
+# Copyright (C) 2010 Canonical, Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License or (at your option) any later version of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+"""Tests for fastimport."""
+
+import unittest
+
+
+def test_suite():
+    names = [
+        'test_commands',
+        'test_errors',
+        ]
+    module_names = ['fastimport.tests.' + name for name in names]
+    result = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromNames(module_names)
+    result.addTests(suite)
+    return result
