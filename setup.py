abe11561f0570cb88f47d7021e1de1f1f2cc487c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-04-15 13:25:49 +0100

    Release 0.9.7.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-version = "0.9.7dev"
+version = "0.9.7"
 
 setup(name="fastimport",
       description="VCS fastimport/fastexport parser",
@@ -11,7 +11,7 @@ setup(name="fastimport",
       maintainer="Jelmer Vernooij",
       maintainer_email="jelmer@jelmer.uk",
       license="GNU GPL v2 or later",
-      url="htps://github.com/jelmer/python-fastimport",
+      url="https://github.com/jelmer/python-fastimport",
       packages=['fastimport', 'fastimport.tests', 'fastimport.processors'],
       scripts=[
           'bin/fast-import-query',
