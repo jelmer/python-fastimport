5cef9e037b7d7b37f58f522ac9ea4e343e6a1dff
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-06-15 16:49:30 +0100

    Add setup.cfg.

--- setup.py
+++ setup.py
@@ -3,30 +3,11 @@ from distutils.core import setup
 
 version = "0.9.8"
 
-setup(name="fastimport",
-      description="VCS fastimport/fastexport parser",
-      version=version,
-      author="Canonical Ltd",
-      author_email="bazaar@lists.canonical.com",
-      maintainer="Jelmer Vernooij",
-      maintainer_email="jelmer@jelmer.uk",
-      license="GNU GPL v2 or later",
-      url="https://github.com/jelmer/python-fastimport",
+setup(version=version,
       packages=['fastimport', 'fastimport.tests', 'fastimport.processors'],
       scripts=[
           'bin/fast-import-query',
           'bin/fast-import-filter',
           'bin/fast-import-info',
-      ],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Operating System :: POSIX',
-          'Topic :: Software Development :: Version Control',
       ],
       )
